b7e5aa6c38cf601333ee45c112a3bc07e53723d5
Mangusstt <Mangusstt@example.com>
2023-05-07 20:48:09 +0700

    Create main.cpp

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,3 +1,35 @@
+//Standard includes
+#include <iostream>
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+using namespace std;
 
-int main() {}
+//Private includes
+#include "game.h"
+
+//Globals
+
+
+int main() {
+  char restart;
+  srand (time(NULL));
+  Game_shell game;
+  for (;;) {
+    game.init();
+    if (game._quit == true) {
+      break;
+    }
+    for (;;) {
+      game.run();
+      // After game ends, offer chance to replay
+      cout << "Play again? (y/N)";
+      cin >> restart;
+      if(restart != 'y') {
+        break;
+      }
+    }
+  }
+  //Clean up - re-enable echo
+  return 0;
+}
